04c806f1eb48fd11201972890bdffcc1bcdb8cc0
Mikulas <rullaf@gmail.com>
2013-08-08 23:26:40 +0200

    always throw exception in grabber

--- src/Csfd/Grabber.php
+++ src/Csfd/Grabber.php
@@ -12,6 +12,11 @@ class Grabber
 	const GET = 'GET';
 	const POST = 'POST';
 
+	const MAX_REDIRECTS = 10;
+	const TIMEOUT = 2;
+
+	const USER_AGENT = 'CSFDAPI Grabber 1.2.1 (csfdapi.cz) mikulas@dite.pro';
+
 
 	/** @var string */
 	protected $url;
@@ -39,12 +44,14 @@ class Grabber
 				'method'  => $http,
 				'header'  => 'Content-type: application/x-www-form-urlencoded',
 				'content' => $http === self::POST ? $query : NULL,
+				'follow_location' => TRUE,
+				'max_redirects' => self::MAX_REDIRECTS,
+				'timeout' => self::TIMEOUT,
 			]
 		]);
 
-		$url = $this->url . $method . ($http === self::GET ? '?' . $query : '');
-		$result = file_get_contents($url, FALSE, $context);
-
+		$url = $this->url . $method . ($query && $http === self::GET ? '?' . $query : '');
+		$result = @file_get_contents($url, FALSE, $context); // @ - throw exception
 		if (!$result) {
 			throw new \RuntimeException('Invalid response received.');
 		}
@@ -58,12 +65,19 @@ class Grabber
 	{
 		$this->queue = new cURL\RequestsQueue;
 		$this->queue->getDefaultOptions()
-			->set(\CURLOPT_TIMEOUT, 5)
+			->set(\CURLOPT_CONNECTTIMEOUT, self::TIMEOUT)
+			->set(\CURLOPT_TIMEOUT, self::TIMEOUT)
 			->set(\CURLOPT_FOLLOWLOCATION, TRUE)
+			->set(\CURLOPT_MAXREDIRS, self::MAX_REDIRECTS)
+			->set(\CURLOPT_USERAGENT, self::USER_AGENT)
 			->set(\CURLOPT_RETURNTRANSFER, TRUE);
 
 		$this->queue->addListener('complete', function (cURL\Event $event) {
-			$this->responses[] = HtmlDomParser::str_get_html($event->response->getContent());
+			$result = $event->response->getContent();
+			if (!$result) {
+				throw new \RuntimeException('Invalid response received.');
+			}
+			$this->responses[] = HtmlDomParser::str_get_html($result);
 		});
 	}
 
