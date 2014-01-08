ffa8d4839a3d8429dcd24a4f31886fa8ad1a3111
Mikulas <rullaf@gmail.com>
2014-01-08 15:23:45 +0100

    Revert "implement entity cache populator"
    This reverts commit 9ee27ef745f6a72983664347032b94aaf7313fd0.

--- tests/cases/Entities/CachingGetterTest.php
+++ tests/cases/Entities/CachingGetterTest.php
@@ -12,11 +12,11 @@ class CachingGetterTest extends TestCase
 	public function testCall_defined()
 	{
 		$foo = new CGImplement;
-		$this->assertSame(0, $foo->callCount);
+		$this->assertSame(0, CGImplement::$callCount);
 		$this->assertSame(CGImplement::RETVAL, $foo->getBar());
-		$this->assertSame(1, $foo->callCount);
+		$this->assertSame(1, CGImplement::$callCount);
 		$this->assertSame(CGImplement::RETVAL, $foo->getBar()); // cached
-		$this->assertSame(1, $foo->callCount);
+		$this->assertSame(1, CGImplement::$callCount);
 	}
 
 	/**
@@ -38,28 +38,6 @@ class CachingGetterTest extends TestCase
 		$this->assertSame('dynamic', $foo->getDynamic());
 	}
 
-	/**
-	 * @covers \Csfd\Entities\CachingGetter::getPropertyFromGetter
-	 */
-	public function testGetPropertyFromGetter()
-	{
-		$e = Access(new CGImplement);
-		$this->assertSame('bar', $e->getPropertyFromGetter('getBar'));
-	}
-
-	/**
-	 * @covers \Csfd\Entities\CachingGetter::setPropertyCache
-	 */
-	public function testSetPropertyCache()
-	{
-		$foo = new CGImplement;
-		$exp = 'value';
-		$foo->setPropertyCache('bar', $exp);
-		$this->assertSame(0, $foo->callCount);
-		$this->assertSame($exp, $foo->getBar());
-		$this->assertSame(0, $foo->callCount);
-	}
-
 }
 
 class CGImplement
@@ -68,11 +46,11 @@ class CGImplement
 
 	const RETVAL = 'value';
 
-	public $callCount = 0;
+	public static $callCount = 0;
 
 	protected function _getBar()
 	{
-		$this->callCount++;
+		self::$callCount++;
 		return self::RETVAL;
 	}
 
